c221b97dd92993fc9b43d645da5fb7be77878a8f
joseph.chiang20 <joseph.chiang20@gmail.com>
2020-04-26 11:01:46 -0400

    add ajax

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -1,3 +1,17 @@
+// $(function(){
+//       $("#footer").load("footer.html");
+//     });
+
 $(function(){
-      $("#footer").load("footer.html");
+  function loadFooter() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("footer").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "footer.html", true);
+    xhttp.send();
+  }
     });
