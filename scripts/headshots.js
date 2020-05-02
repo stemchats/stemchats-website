92699c7d74d4322c29eb143cc6b233ee380f165f
joseph.chiang20 <joseph.chiang20@gmail.com>
2020-05-02 14:53:44 -0400

    update headshot scripts

--- scripts/headshots.js
+++ scripts/headshots.js
@@ -82,32 +82,6 @@ loadExecutives = () => {
 }
 loadExecutives();
 
-      // if(i==0||i==1) {
-      // attributes[0][i] = document.createElement(attributes[1][i]);
-      // attributes[0][i].className = attributes[2][i];
-      //  if(i==0) {
-      //     loadExec.appendChild(attributes[0][i]);
-      //   } else if(i==1){
-      //     attributes[0][i-1].appendChild(attributes[0][i]);
-      //   }
-      // } else if(i>1 && i<attributes[0].length) {  //if iterator value is between 2-6
-      //   for(let k=0;k<executives[0].length;k++) { //for each executive value, run the code below
-      //     attributes[0][i] = document.createElement(attributes[1][i]);
-      //     if(i<5) { // for i values 2,3,4. applies next 4 sets of classes to respective elements
-      //       attributes[0][i].className = attributes[2][i];
-      //       attributes[0][i-1].appendChild(attributes[0][i]); //append created element in the parent element (element before it in the array)
-      //     } else if(i==5||i==6) { //for i values 5,6 (h3, p)
-      //       attributes[0][i].className = attributes[2][i];
-      //       attributes[0][4].appendChild(attributes[0][i]);
-      //     }
-      //           attributes[0][i].innerHTML = executives[0][k];
-      //           attributes[0][i].innerHTML = executives[1][k];
-      //
-      //     }
-      //
-      //   }
-
-
 
 function loadLeads() {
 
@@ -115,38 +89,13 @@ function loadLeads() {
 function loadCoordinators(){
 
 }
-//     let pokeList = document.getElementById('pokelist');
-//
-//     //Creates rows
-//     // let row = document.createElement('div');
-//     // row.className = "row";
-//     // pokeList.appendChild(row);
-//
-//     let row = null;
-//
-//     let pokemonCounter = 0;
-//
-//     if(request.status >= 200 && request.status < 400 ){
-//         data.results.forEach(pokemon => {
-//
-//             if(pokemonCounter % 4 == 0){
-//                 row = document.createElement('div');
-//                 row.className = "row";
-//                 pokeList.appendChild(row);
-//             }
-//
-//
-//
-//             let card = document.createElement('div');
-//             card.className = "col-3 pokemon";
-//
-//             let p = document.createElement('p');
-//             p.textContent = pokemon.name;
-//             card.appendChild(p);
-//             row.appendChild(card);
-//             pokemonCounter++;
-//         });
-//
-//
-//     }
-// };
+//example html
+<div class = "col-md-4">
+    <div class="card teamcard" style="width: 100%">
+        <img class="card-img-top teamcard-img" src="/images/headshots/MinhPhucTran.jpg" alt="Card image cap">
+        <div class="card-body teamcard-body">
+            <h3 class="card-title teamcard-title">Minh Phuc Tran</h3>
+            <p class="card-text teamcard-description">Founder & CEO</p>
+        </div>
+    </div>
+</div>
