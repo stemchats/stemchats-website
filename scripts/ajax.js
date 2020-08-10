98595977c5317fc778af0821f933a6898f6fc0e7
joseph.chiang20 <joseph.chiang20@gmail.com>
2020-08-10 15:25:35 -0700

    create header component

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -9,4 +9,18 @@ function loadFooter() {
     request.open("GET", "/footer.html", true);
     request.send();
   }
+
+function loadHeader() {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("header").innerHTML =
+        this.responseText;
+      }
+    };
+    request.open("GET", "/header.html", true);
+    request.send();
+  }
+
 loadFooter();
+loadHeader();
