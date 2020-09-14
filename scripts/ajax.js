9000c5e3dd5729b4f1f61903fd4adbbf5427f562
koienshi <55773406+koienshi@users.noreply.github.com>
2020-09-14 02:44:00 -0400

    centered arrow

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -24,3 +24,12 @@ function loadHeader() {
 
 loadFooter();
 loadHeader();
+
+
+
+
+//arrow: black --> transparent
+window.addEventListener("scroll", function() {
+  var arrow = document.querySelector("arrow");
+  arrow.classList.toggle("gone", window.scrollY > 50);
+})
