70acd962bc900b446ed49d01998bb26d244081bd
joseph.chiang20 <joseph.chiang20@gmail.com>
2021-04-12 09:25:45 -0700

    add note instructions

--- scripts/opportunities.js
+++ scripts/opportunities.js
@@ -1,3 +1,6 @@
+/* NOTE: This script file has been linked to the get-involved page, however, since we are not launching this feature yet,
+please push to the separate branch called "test" */
+
 // generate cards
 
 
