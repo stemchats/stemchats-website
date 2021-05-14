9b104fb0296b659a4e8ea954a58d1b37774e357c
sabrinaachang <chang.sabrina2004@gmail.com>
2021-05-13 21:15:13 -0400

    faq modal

--- scripts/opportunities.js
+++ scripts/opportunities.js
@@ -58,36 +58,6 @@ const opportunities = document.getElementById("opportunities");
 // const department = document.getElementById("department");
 const animate = document.getElementById("opportunities_container");
 
-let faqs = [
-    {
-        question: "Do I have to identify as underrepresented in STEM to apply?",
-        answer: "STEMchats’s Mission is to shatter barriers in the STEM fields through impactful programming for all students, especially those who are underserved and underrepresented in STEM. Students underrepresented in the STEM fields are those coming from, but not limited to, first-generation and low-income backgrounds, as well as minorities including women, people of color, LGBTQ+, students with disabilities, students attending rural and/or underfunded high schools. Thus, please keep in mind that preference will be given to students belonging to the aforementioned groups to be a part of our team. Additionally, STEMchats will be evaluating all applications by stripping them of identifying information.<br><br>EEO Statement: At STEMchats, our mission is to help shatter barriers in the STEM fields for racially, economically, and otherwise underrepresented students. We strive to impact these diverse communities, and to create a diverse team that represents them. STEMchats is committed to being inclusive to applicants and people of all backgrounds to grow together."
-    }, {
-        question: "What do you look for in applicants?",
-        answer: "STEMchats thrive on its team members’ individuality, but there are characteristics shared by everyone, including a passion for our mission, willingness to learn and take initiative, being a good team player, and having good communication skills. It is equally as important for all of our team members to have these characteristics. Additionally, we consider each applicant’s overall “fit” for each team; you can check out an overview of our team culture <a href=\"https://tinyurl.com/SCWorkplaceBenefits\" target=\"_blank\">here</a>. Make sure to go through our entire website to learn about all that the work that our organization does!"
-    }, {
-        question: "What is the time commitment for a STEMchats intern?",
-        answer: "Though time commitment may vary slightly depending on the week as well as the team, all Interns are expected to contribute a minimum of six hours of their time per week to STEMchats, which includes team meetings, time working on projects, and attending workshops."
-    }, {
-        question: "What is the recruitment timeline? When will I know the application decision?",
-        answer: "STEMchatsThe application will be open from April 16th to May 24th (closes at 11:59 PM EST). We will release first-round decisions on May 28th, interviews will be held from May 30th to June 4th, and final decisions will be released on June 6th. All selected interns must respond with a final decision by June 8th, 2021 at 11:59pm EST. thrive on its team members’ individuality, but there are characteristics shared by everyone, including a passion for our mission, willingness to learn and take initiative, being a good team player, and having good communication skills. It is equally as important for all of our team members to have these characteristics. Additionally, we consider each applicant’s overall “fit” for each team; you can check out an overview of our team culture <a href=\"https://tinyurl.com/SCWorkplaceBenefits\" target=\"_blank\">here</a>. Make sure to go through our entire website to learn about all that the work that our organization does!"
-    }, {
-        question: "What will you ask during the interview?",
-        answer: "Interview details will be provided in the invitations, but interviews are a casual conversation with a Leadership Team member, the respective Lead of the team to which you applied, or a Senior Advisor who will ask about your application responses, your background and experiences, and interest in STEMchats! There will only be two interviewers present at your interview!"
-    }, {
-        question: "Are team members paid?",
-        answer: "STEMchats team members contribute on a volunteer-basis; unfortunately, we are unable to pay our team members. We aim to be as transparent as possible around this issue because for low-income applicants, we understand that dedicating time to an organization without monetary compensation is a privilege. We hope the work you would do at STEMchats can provide a unique learning experience, and the Leadership Team will provide official verification of hours worked for any team member who requests logged volunteer hours!"
-    }, {
-        question: "What is STEMchats' submission policy?",
-        answer: "Due to applicant volume and our recruitment timeline, we will not be accepting any late applications. For this reason, if you believe you will be unable to submit your application over Google Forms for technical reasons, please contact us at stemchats@stemchats.org BEFORE the 11:59 PM EST deadline on May 24th, 2021."
-    }, {
-        question: "Do I have to have a STEM background to apply?",
-        answer: "We encourage anyone with an interest in STEM or business-related fields (i.e grant-writing, marketing, outreach etc.) to apply! Since we target students who might not have had extensive opportunities to pursue STEM, we are in no way expecting a deep STEM background. We hope the application allows you to express how your experience and perspective can be of value for STEMchats; we do not have a perfect applicant in mind and many of our current team members did not have a comprehensive STEM background when applying."
-    }, {
-        question: "Am I still eligible to apply if I'm taking a gap year during the 2021-2022 year (or are a part-time student)",
-        answer: "Of course. Due to the situation surrounding COVID-19, we will accept applications from anyone who is taking a break from being a student. In fact, we encourage those who will have more free time to apply because it reflects initiative in growing personally and professionally."
-    },
-];
 createFAQs = (f, container) => {
     var count=0;
     f.forEach((j) => {
@@ -96,7 +66,7 @@ createFAQs = (f, container) => {
         faq.className = "accordion intern-faq"; 
         faq.id = "accordianExample";
         var card = document.createElement("div");
-        card.className = "card intern-faq-question";
+        card.className = "intern-faq-question";
         var q = document.createElement("div");
         q.className = "card-header intern-faq-question"; 
         q.id = "headingOne";
@@ -119,6 +89,8 @@ createFAQs = (f, container) => {
         card.appendChild(q);
         faq.appendChild(card); 
 
+        card.addEventListener("click", expand); 
+
         // card2 = document.createElement("div"); 
         // card2.className = "collapse"; 
         var numwords = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
@@ -128,6 +100,32 @@ createFAQs = (f, container) => {
         card3.className = "collapse";
         card3.setAttribute("aria-labelledby", "heading"+numwords[count]);
         card3.setAttribute("data-parent", "#accordionExample");
+
+        function expand(){
+            console.log("clicked");
+            // let expanded = false;
+            // if (card3.classList == "collapse show") {
+            //     expanded = true;
+            // } else { expanded = false; }
+            
+            card3.className = "collapsing";
+
+            setTimeout(function() {
+                // if (!expanded) {
+
+                // }
+                ques.classList.toggle("collapsed");
+
+                card3.className = "collapse show";
+    
+                if (ques.getAttribute("aria-expanded") == "false") {
+                    ques.setAttribute("aria-expanded", "true");
+                } else {
+                    ques.setAttribute("aria-expanded", "false");
+                    card3.className = "collapse";
+                }
+            }, 200)
+        }
 
         // card3 = "<div id=\"collapseOne\" class=\"collapse\" aria-labelledby=\"headingOne\" data-parent=\"#accordionExample\">"
         var ans = document.createElement("p");
@@ -253,14 +251,14 @@ createOpportunities = (opps) => {
             container.appendChild(app);
 
             
-            if (opps[i].title == "2021 STEMchats Internship Program"){
+            if (opps[i].faq){
                 //faq section
-                faq = document.createElement("h3"); 
-                faq.className = "center name"; 
-                faq.innerHTML = "FAQs";
-                container.appendChild(faq); 
+                f = document.createElement("h3"); 
+                f.className = "center name"; 
+                f.innerHTML = "FAQs";
+                container.appendChild(f); 
 
-                window.onload = createFAQs(faqs, container);
+                window.onload = createFAQs(opps[i].faq, container);
                 // container.appendChild(div4);
             }
 
@@ -323,7 +321,37 @@ let opportunitiesObj = [
         category: "Internship",
         description: "Thank you for your interest in our Second Annual Internship Program! We encourage you to submit an application to join us this summer! Please make sure to read all parts of the Google Form, the linked Google Docs, and FAQs at the bottom of this page carefully. Please email us at <a href = \"mailto: stemchats@stemchats.org\" target = \"_blank\">stemchats@stemchats.org</a> if you have any questions! We are excited to provide a summer of engaging and fulfilling work, networking, events, and more as we work towards our mission of shatter barriers in STEM for underrepresented students.",
         src: "/images/internship_shared_workspace.png",
-        application: "https://docs.google.com/forms/d/190P98goeJ2EeC0-GmAsHnR0oW6mm0zfnhnpuX4And_I/viewform?edit_requested=true"
+        application: "https://docs.google.com/forms/d/190P98goeJ2EeC0-GmAsHnR0oW6mm0zfnhnpuX4And_I/viewform?edit_requested=true",
+        faq: [
+            {
+                question: "Do I have to identify as underrepresented in STEM to apply?",
+                answer: "STEMchats’s Mission is to shatter barriers in the STEM fields through impactful programming for all students, especially those who are underserved and underrepresented in STEM. Students underrepresented in the STEM fields are those coming from, but not limited to, first-generation and low-income backgrounds, as well as minorities including women, people of color, LGBTQ+, students with disabilities, students attending rural and/or underfunded high schools. Thus, please keep in mind that preference will be given to students belonging to the aforementioned groups to be a part of our team. Additionally, STEMchats will be evaluating all applications by stripping them of identifying information.<br><br>EEO Statement: At STEMchats, our mission is to help shatter barriers in the STEM fields for racially, economically, and otherwise underrepresented students. We strive to impact these diverse communities, and to create a diverse team that represents them. STEMchats is committed to being inclusive to applicants and people of all backgrounds to grow together."
+            }, {
+                question: "What do you look for in applicants?",
+                answer: "STEMchats thrive on its team members’ individuality, but there are characteristics shared by everyone, including a passion for our mission, willingness to learn and take initiative, being a good team player, and having good communication skills. It is equally as important for all of our team members to have these characteristics. Additionally, we consider each applicant’s overall “fit” for each team; you can check out an overview of our team culture <a href=\"https://tinyurl.com/SCWorkplaceBenefits\" target=\"_blank\">here</a>. Make sure to go through our entire website to learn about all that the work that our organization does!"
+            }, {
+                question: "What is the time commitment for a STEMchats intern?",
+                answer: "Though time commitment may vary slightly depending on the week as well as the team, all Interns are expected to contribute a minimum of six hours of their time per week to STEMchats, which includes team meetings, time working on projects, and attending workshops."
+            }, {
+                question: "What is the recruitment timeline? When will I know the application decision?",
+                answer: "STEMchatsThe application will be open from April 16th to May 24th (closes at 11:59 PM EST). We will release first-round decisions on May 28th, interviews will be held from May 30th to June 4th, and final decisions will be released on June 6th. All selected interns must respond with a final decision by June 8th, 2021 at 11:59pm EST. thrive on its team members’ individuality, but there are characteristics shared by everyone, including a passion for our mission, willingness to learn and take initiative, being a good team player, and having good communication skills. It is equally as important for all of our team members to have these characteristics. Additionally, we consider each applicant’s overall “fit” for each team; you can check out an overview of our team culture <a href=\"https://tinyurl.com/SCWorkplaceBenefits\" target=\"_blank\">here</a>. Make sure to go through our entire website to learn about all that the work that our organization does!"
+            }, {
+                question: "What will you ask during the interview?",
+                answer: "Interview details will be provided in the invitations, but interviews are a casual conversation with a Leadership Team member, the respective Lead of the team to which you applied, or a Senior Advisor who will ask about your application responses, your background and experiences, and interest in STEMchats! There will only be two interviewers present at your interview!"
+            }, {
+                question: "Are team members paid?",
+                answer: "STEMchats team members contribute on a volunteer-basis; unfortunately, we are unable to pay our team members. We aim to be as transparent as possible around this issue because for low-income applicants, we understand that dedicating time to an organization without monetary compensation is a privilege. We hope the work you would do at STEMchats can provide a unique learning experience, and the Leadership Team will provide official verification of hours worked for any team member who requests logged volunteer hours!"
+            }, {
+                question: "What is STEMchats' submission policy?",
+                answer: "Due to applicant volume and our recruitment timeline, we will not be accepting any late applications. For this reason, if you believe you will be unable to submit your application over Google Forms for technical reasons, please contact us at stemchats@stemchats.org BEFORE the 11:59 PM EST deadline on May 24th, 2021."
+            }, {
+                question: "Do I have to have a STEM background to apply?",
+                answer: "We encourage anyone with an interest in STEM or business-related fields (i.e grant-writing, marketing, outreach etc.) to apply! Since we target students who might not have had extensive opportunities to pursue STEM, we are in no way expecting a deep STEM background. We hope the application allows you to express how your experience and perspective can be of value for STEMchats; we do not have a perfect applicant in mind and many of our current team members did not have a comprehensive STEM background when applying."
+            }, {
+                question: "Am I still eligible to apply if I'm taking a gap year during the 2021-2022 year (or are a part-time student)",
+                answer: "Of course. Due to the situation surrounding COVID-19, we will accept applications from anyone who is taking a break from being a student. In fact, we encourage those who will have more free time to apply because it reflects initiative in growing personally and professionally."
+            },
+        ]
     }
     // }, {
     //     title: "Summer Research Program",
