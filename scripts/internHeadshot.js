815323dbee6460fe781f9afde471953827b38ebb
anastasia21112 <anad221122@gmail.com>
2021-07-02 22:44:38 -0600

    intern page header and photos

--- scripts/internHeadshot.js
+++ scripts/internHeadshot.js
@@ -2,16 +2,280 @@
 //interns
 let internsObj = [
 
-	// {
-	// 	name: "",
-	// 	position: "",
-	// 	linkedIn: '',
-	// 	home: "",
-	// 	src: "",
-	// 	bio: ""
-  // }
+	{
+		name: "Sabina Sitwat",
+		position: "",
+		linkedIn: '',
+		home: "",
+		src: "SabinaSitwat.JPG",
+		bio: ""
+  }, 
+  {
+	name: "Vanessa Gong",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "VanessaGong.JPG",
+	bio: ""
+   },
+   {
+	name: "Sana Nauman",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "SanaNauman.JPG",
+	bio: ""
+   },
+   {
+	name: "Elaine Viloria",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "ElaineViloria.JPG",
+	bio: ""
+   },
+   {
+	name: "Jessica Maher",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "JessicaMaher.JPG",
+	bio: ""
+   },
+   {
+	name: "Melissa Bunnapradist",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "MelissaBunnapradist.JPG",
+	bio: ""
+   },
+   {
+	name: "Cecilia Nguyen",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "CeciliaNguyen.JPG",
+	bio: ""
+   },
+   {
+	name: "Hlla Shehata",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "HllaShehata.png",
+	bio: ""
+   },
+   {
+	name: "Myangelia Gonzalez",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "MyangeliaGonzalez.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Nafisa Hossain",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "NafisaHossain.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Nafisa Hossain",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "NafisaHossain.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Lauryne Assa",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "LauryneAssa.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Michelle Lee",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "MichelleLee.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Sue Yim",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "SueYim.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Katie Zhu",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "KatieZhu.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Kevin Chen",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "KevinChen.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Evan Lin",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "EvanLin.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Kenny Phan",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "KennyPhan.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Hannah Zhang",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "HannahZhang.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Neha Bhardwaj",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "NehaBhardwaj.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Victoria Salazar",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "VictoriaSalazar.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Jessica Dett",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "JessicaDett.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Ryan Bui",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "RyanBui.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Saba Fayyazi",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "SabaFayyazi.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Kaliah Williams",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "KaliahWilliams.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Roy Park",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "RoyPark.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Dyllan Eslava",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "DyllanEslava.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Cynthia Fayssal",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "CynthiaFayssal.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Maya Lall",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "MayaLall.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Layla Salam",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "LaylaSalam.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Asmita Malakar",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "AsmitaMalakar.png",
+	bio: ""
+   }, 
+   {
+	name: "Zaryaab Khan",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "ZaryaabKhan.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Hiba Hamad",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "HibaHamad.JPG",
+	bio: ""
+   }, 
+   {
+	name: "Juliet Amadi",
+	position: "",
+	linkedIn: '',
+	home: "",
+	src: "JulietAmadi.JPG",
+	bio: ""
+   }
 ]
 
 //load all at onload
-// window.onload = createMembers(internsObj);
-// window.onload =	animateOnload();
+window.onload = createMembers(internsObj);
+window.onload =	animateOnload();
