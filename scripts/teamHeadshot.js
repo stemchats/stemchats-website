3cad6ffcd7542b5aad062e34889c3c5617df8475
joseph.chiang20 <joseph.chiang20@gmail.com>
2021-11-07 23:50:04 -0800

    update Joseph bio

--- scripts/teamHeadshot.js
+++ scripts/teamHeadshot.js
@@ -50,7 +50,7 @@ let membersObj = [
 	home: "Vancouver, Canada",
 	team: "Technology",
 	src: "JosephChiang.jpg",
-	bio: "Joseph is an aspiring computer scientist and a rising college freshman in Canada. He joined STEMchats to support and prepare the next and current generation, especially low-income students, for impactful real world opportunities through immersive and relevant programs in STEM. When he is not learning about technology, he enjoys playing chess, writing poetry, and chilling to lofi beats."
+	bio: "Joseph is an aspiring computer scientist and a sophomore at University of British Columbia. He joined STEMchats to support and prepare the next and current generation, especially low-income students, for impactful real world opportunities through immersive and relevant programs in STEM. When he is not learning about technology, he enjoys exploring nature, writing poetry, and relaxing to lofi beats."
 }, {
 	name: "MyCo Le",
 	position: "Technology Lead",
